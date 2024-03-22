3b4505c7cc8e6f89c3edfe032880b46ae6682919
Maxime Vande <Maxime62580@laposte.net>
2024-03-22 22:24:10 +0100

    A

--- src/components/Debut.js
+++ src/components/Debut.js
@@ -1,93 +1,78 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Canvas, useLoader } from '@react-three/fiber';
 import { OrbitControls, Box, Plane, Text } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
+import { TextureLoader } from 'three/src/loaders/TextureLoader'; // Importez TextureLoader pour charger des images
 
 function Room() {
-    // Charger les textures 3D depuis différents fichiers GLB
-    const wallTextureGLB = useLoader(GLTFLoader, './brique.gltf');
-    const ceilingTextureGLB = useLoader(GLTFLoader, './brique.gltf');
-    const floorTextureGLB = useLoader(GLTFLoader, './brique.gltf');
+    // Charger la texture JPG
+    const textureJPG = useLoader(TextureLoader, '/mur.jpg');
 
-    // Définir les coordonnées XYZ de la position du modèle et l'échelle du modèle glTF
-    const gltfPosition = [250, 0, 0];
-    const gltfScale = [0, 0, 0];
-
-    // Extraire les textures 3D des modèles GLTF
-    const wallTexture = useRef();
-    const ceilingTexture = useRef();
-    const floorTexture = useRef();
-
-    wallTextureGLB.scene.traverse((child) => {
-        if (child.isMesh) {
-            wallTexture.current = child.material.map;
-        }
-    });
-
-    ceilingTextureGLB.scene.traverse((child) => {
-        if (child.isMesh) {
-            ceilingTexture.current = child.material.map;
-        }
-    });
-
-    floorTextureGLB.scene.traverse((child) => {
-        if (child.isMesh) {
-            floorTexture.current = child.material.map;
-        }
-    });
+    // Utilisez la texture JPG pour tous les matériaux
+    const wallTexture = textureJPG;
+    const ceilingTexture = textureJPG;
+    const floorTexture = textureJPG;
 
     return (
         <>
             {/* Mur gauche */}
             <Box args={[1, 400, 1000]} position={[-500, 0, 0]}>
-                <meshStandardMaterial map={wallTexture.current} />
+                <meshStandardMaterial map={wallTexture} />
             </Box>
 
             {/* Mur droit */}
             <Box args={[1, 400, 1000]} position={[500, 0, 0]}>
-                <meshStandardMaterial map={wallTexture.current} />
+                <meshStandardMaterial map={wallTexture} />
             </Box>
 
             {/* Mur arrière */}
             <Box args={[1000, 400, 1]} position={[0, 0, -500]}>
-                <meshStandardMaterial map={wallTexture.current} />
+                <meshStandardMaterial map={wallTexture} />
             </Box>
 
             {/* Mur avant */}
             <Box args={[1000, 400, 1]} position={[0, 0, 500]}>
-                <meshStandardMaterial map={wallTexture.current} />
+                <meshStandardMaterial map={wallTexture} />
             </Box>
 
             {/* Sol */}
             <Plane args={[1000, 1000]} position={[0, -200, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-                <meshStandardMaterial map={floorTexture.current} />
+                <meshStandardMaterial map={floorTexture} />
             </Plane>
 
             {/* Plafond */}
             <Plane args={[1000, 1000]} position={[0, 200, 0]} rotation={[Math.PI / 2, 0, 0]}>
-                <meshStandardMaterial map={ceilingTexture.current} />
+                <meshStandardMaterial map={ceilingTexture} />
             </Plane>
+            {/* Colonne */}
+            <mesh position={[-300, 0, -200]}>
+                <cylinderGeometry args={[20, 20, 400, 32]} />
+                <meshStandardMaterial color="white" />
+            </mesh>
 
-            {/* Placer le modèle glTF dans un groupe pour le garder fixe par rapport à la scène */}
-            <group position={gltfPosition}>
-                <primitive object={wallTextureGLB.scene} scale={gltfScale} />
-                <primitive object={ceilingTextureGLB.scene} scale={gltfScale} />
-                <primitive object={floorTextureGLB.scene} scale={gltfScale} />
-            </group>
+            {/* Statue */}
+            <mesh position={[0, 0, 300]}>
+                <sphereGeometry args={[100, 32, 32]} />
+                <meshStandardMaterial color="marble" />
+            </mesh>
+
+            {/* Table */}
+            <mesh position={[400, 50, -100]}>
+                <boxGeometry args={[200, 100, 100]} />
+                <meshStandardMaterial color="brown" />
+            </mesh>
             {/* Pour rendre un élément cliquable */}
             <Text
-                position={[0, 0, -10]}
-                fontSize={5}
-                color="blue"
+                position={[0, 0, -250]}
+                fontSize={10}
+                color="red"
                 anchorX="center"
                 anchorY="middle"
                 onClick={() => {
                     // Rediriger vers une autre page au clic
-                    window.location.href = '/autre-page';
+                    window.location.href = '/Home2';
                 }}
             >
-                Commencer à jouer
+                Prêt?
             </Text>
         </>
     );
